410e5a6f2f46a10abf0a44b88086a5517cb81ade
Mikkel Andersen <120143751+MikkelsDesign@users.noreply.github.com>
2025-03-03 09:04:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// window.addEventListener('scroll', function () {
-//   var header = document.getElementById('site-header');
-//   if (window.scrollY > 750) {
-//     header.classList.add('scrolled');
-//   } else {
-//     header.classList.remove('scrolled');
-//   }
-// });
-
 
 // Countdown script
 
