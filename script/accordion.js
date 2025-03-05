3cf061b20db9b0f7a6f02d8d5a1842e35bbcb054
Mikkel Andersen <120143751+MikkelsDesign@users.noreply.github.com>
2025-03-05 09:23:36 +0100

    Validering

--- script/accordion.js
+++ script/accordion.js
@@ -9,5 +9,12 @@ accordionHeaders.forEach(header => {
  
 // Funktion til at toggle accordion
 function toggleAccordion(event) {
+
+    const icon = event.currentTarget.querySelector('.icon'); // Finder ikonet i den aktuelle header
+    icon.classList.toggle('menuAaben');
+    
     const content = event.currentTarget.nextElementSibling; // Finder det tilhørende indhold
     content.classList.toggle('visMenu'); }// Åbner eller lukker menuen (aktivere/slukker for classen visMenu)
+
+    
+
